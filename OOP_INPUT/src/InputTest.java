aa907d5b49d81948a374fad784c9b89bbf2bf74a
temarsl <arslanovamaryam@gmail.com>
2022-11-18 12:59:54 +0300

    декоратор ввода-вывода

---
+++ OOP_INPUT/src/InputTest.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class InputTest {
+    public static void main(String[] args) throws IOException {
+        int c;
+        InputStream in = null;
+        try {
+             in =
+                    new LowerCaseInputStream(
+                            new BufferedInputStream(
+                                    new FileInputStream("test.txt")));
+
+            while ((c = in.read()) >= 0) {
+                System.out.print((char) c);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+    }
+}
